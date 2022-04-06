0747f4c9508cebd95334dbcb6c2b03121ec8bd4c
Kusakin_Sl <kusakin.slava@inbox.ru>
2022-04-06 16:00:31 +0300

    Initial Commit

---
+++ Constraints/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Constraints
+//
+//  Created by Вячеслав Кусакин on 06.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
